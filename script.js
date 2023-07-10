de869260b8ff189279f586fabedef6ac9e3b1d19
isomer04 <isomer008@gmail.com>
2023-07-10 15:44:33 -0400

    Added a different background color and copied to the clipboard button.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var resultCounter = 1; // Counter to keep track of the result number
+
 function textExtract() {
   // Get the user input text
   var userInput = document.getElementById("input-text").value;
@@ -13,7 +15,60 @@ function textExtract() {
   // Collapse consecutive newlines into a single newline
   extractedText = extractedText.replace(/\n{2,}/g, "\n\n");
 
+  // // Display the extracted text on the page
+  // var extractedTextDiv = document.getElementById("extractedText");
+  // extractedTextDiv.innerHTML = extractedText.replace(/\n/g, "<br>");
+
   // Display the extracted text on the page
-  var extractedTextDiv = document.getElementById("extractedText");
-  extractedTextDiv.innerHTML = extractedText.replace(/\n/g, "<br>");
+ // Display the extracted text on the page
+ var extractedTextDiv = document.getElementById("extractedText");
+
+ // Create a new list item for the extracted text
+ var listItem = document.createElement("li");
+ listItem.innerText = extractedText;
+ listItem.setAttribute("id", "result-" + resultCounter);
+ listItem.addEventListener("click", function () {
+   listItem.style.backgroundColor = getRandomColor();
+ });
+
+ // Create a "Copy to Clipboard" button
+ var copyButton = document.createElement("button");
+ copyButton.innerText = "Copy to Clipboard";
+ copyButton.addEventListener("click", function () {
+   copyTextToClipboard(extractedText);
+ });
+
+ // Append the copy button to the list item
+ listItem.appendChild(copyButton);
+
+ // Check if there are existing results
+ var existingResults = extractedTextDiv.getElementsByTagName("li");
+ if (existingResults.length > 0) {
+   // Insert the new list item before the first existing result
+   extractedTextDiv.insertBefore(listItem, existingResults[0]);
+ } else {
+   // Append the new list item to the existing results
+   extractedTextDiv.appendChild(listItem);
+ }
+
+ // Increment the result counter for the next result
+ resultCounter++;
+}
+
+function getRandomColor() {
+ var letters = "0123456789ABCDEF";
+ var color = "#";
+ for (var i = 0; i < 6; i++) {
+   color += letters[Math.floor(Math.random() * 16)];
+ }
+ return color;
+}
+
+function copyTextToClipboard(text) {
+ var tempInput = document.createElement("input");
+ tempInput.value = text;
+ document.body.appendChild(tempInput);
+ tempInput.select();
+ document.execCommand("copy");
+ document.body.removeChild(tempInput);
 }
